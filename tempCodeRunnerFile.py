e66768ec9f1c1203c05b866f47a07e328d7ae026
CHANDAN PAL <chandanpal9988@gmail.com>
2025-09-05 21:06:44 +0530

    Add files via upload

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,3 @@
+# #===title=====#
+# title = Label(self.root,text="Student Result Management System",padx=20,compound=LEFT,image=self.logo_dash,font=("goudy old style",20,"bold"),bg="#033054",fg="white").place(x=0,y=0,relwidth=1,height=50)
+# # ===MENU====
